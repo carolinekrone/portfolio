d08569f0a9a045151f5556179ff1ae08cdd8c2f4
caroline <kronecaroline@gmail.com>
2019-12-18 13:20:06 -0300

    Updates

---
+++ precache-manifest.88a48dfc44c928d75b1d7a93862370e7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab9363ff344c602f55ae7ff4da4a8852",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "e96993e891a76c44bede",
+    "url": "/portfolio/static/css/main.b1a799af.chunk.css"
+  },
+  {
+    "revision": "44b92462072bfe29ba4b",
+    "url": "/portfolio/static/js/2.4a62ac79.chunk.js"
+  },
+  {
+    "revision": "e96993e891a76c44bede",
+    "url": "/portfolio/static/js/main.dc8eb8ec.chunk.js"
+  },
+  {
+    "revision": "6b26cb3bc1b9147ba086",
+    "url": "/portfolio/static/js/runtime-main.86a05020.js"
+  }
+]);
