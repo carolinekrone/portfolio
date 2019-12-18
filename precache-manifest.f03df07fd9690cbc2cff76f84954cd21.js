2223858178c66d125aa66b1a815f1334a586447c
caroline <kronecaroline@gmail.com>
2019-12-18 12:51:44 -0300

    Updates

---
+++ precache-manifest.f03df07fd9690cbc2cff76f84954cd21.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "839878e35a2a82301766f9c4a79e0c2e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7f67fede84e933146ade",
+    "url": "/static/css/main.b1a799af.chunk.css"
+  },
+  {
+    "revision": "44b92462072bfe29ba4b",
+    "url": "/static/js/2.4a62ac79.chunk.js"
+  },
+  {
+    "revision": "7f67fede84e933146ade",
+    "url": "/static/js/main.2d9bc0e9.chunk.js"
+  },
+  {
+    "revision": "0ac2b0fd5fd4af3be109",
+    "url": "/static/js/runtime-main.8896086d.js"
+  }
+]);
